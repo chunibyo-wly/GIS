d185f868831b5dcfcbf3a3f52189544fa3e8adc9
chunibyo-wly <chunibyo.wly@gmail.com>
2019-08-22 21:46:45 +0800

    19/08/22

--- tools/spider.py
+++ tools/spider.py
@@ -7,25 +7,34 @@ target_url = "http://www.zhuatongji.com/wszttjf/list_3_"
 
 image_url = "http://supcache.airbdata.cn/data/www.zhuatongji.com/"
 
+srcSet = []
+
 
 def download(file_name, Photo_URL):
     Picture_request = requests.get(image_url + Photo_URL)
     if Picture_request.status_code == 200:
-        with open('./photo/' + file_name, 'wb') as f:
+        with open('../static/photo/' + file_name, 'wb') as f:
             f.write(Picture_request.content)
 
 
 if __name__ == "__main__":
     i = 1
     page = 1
-    try:
-        for page in range(1, 30):
+
+    for page in range(0, 75):
+        try:
             soup = BeautifulSoup(
                 requests.get(target_url + str(page) + ".html", timeout=5).text,
                 'html.parser')
             for img in soup.select('#col1 > div:nth-child(1) > ul img'):
-                download(str(i) + ".png", img.get("src"))
-                # print(img.get("src"))
-                i = i + 1
-    except Exception:
-        print("page", page, "i", i)
+                src = img.get("src")
+                if src in srcSet:
+                    pass
+                else:
+                    download(str(i) + ".png", src)
+                    srcSet.append(src)
+                    i = i + 1
+        except Exception:
+            print(Exception.__traceback__)
+            print("page", page, "i", i)
+            continue
