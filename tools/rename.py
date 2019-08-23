2fa807cba250283ae1297038e2d2e6eb149e0c4a
chunibyo-wly <chunibyo.wly@gmail.com>
2019-08-23 17:28:40 +0800

    d

--- tools/rename.py
+++ tools/rename.py
@@ -2,7 +2,7 @@
 
 import os
 
-path = input('../static/photo/')
+path = '/home/chunibyo/Documents/Code/GIS/static/photo/'
 # 获取该目录下所有文件，存入列表中
 f = os.listdir(path)
 n = 0
@@ -11,7 +11,8 @@ for i in f:
         # 设置旧文件名（就是路径+文件名）
         oldname = path + f[n]
         # 设置新文件名
-        newname = path + str(n + 1) + ".png"
+        newname = path + str(n + 1) + "backpack.png"
+        print(newname)
         # 用os模块中的rename方法对文件改名
         os.rename(oldname, newname)
         n += 1
